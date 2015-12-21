58bf1d51ae3e55ce4989da72d45be5f7304b7726
Julia <loboda.yuliya@gmail.com>
2015-12-21 18:47:49 +0200

    create class verification for need login and check big card

---
+++ src/test/java/automation/main_checklist/main_points.java
@@ -0,0 +1,41 @@
+package automation.main_checklist;
+
+import automation.Constants;
+import automation.TestHelper;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Julia on 21.12.2015.
+ */
+public class main_points extends TestHelper {
+    /**
+     *
+     */
+
+    //проверить открытие страницки эксперта на главной
+
+
+    @Test
+    public void VerificationForNeedLoginFronBigExpertCard() {
+        WebDriver window = getNewWindow();
+        window.get(Constants.SITE_URL);
+
+        //найти эксперта со статусом Доступен сейчас
+        //pauseUntilDisplayed(By.className("card__status-inner"),window);
+        window.findElement(By.className("card__image card__image_round")).click();
+
+        //pauseUntilDisplayed подождать пока окно большой карты Эксперта появилось
+        pauseUntilDisplayed(By.xpath("//*[@class='product__title']"),window);
+
+        //кликнуть на кнопку Позвонить
+        window.findElement((By.xpath("//*[@class='ga_btn btn btn-green btn-lg product__order-button']"))).click();
+
+
+        //подождать пока появиться форма логина
+        pauseUntilDisplayed(By.xpath("//*[@class='login']"),window);
+
+
+          }
+}
