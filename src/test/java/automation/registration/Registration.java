197b12e67116969c17e7c4e9fa3588c852b3f143
Roman Ugolnikov <r.uholnikov@levi9.com>
2016-01-19 16:22:37 +0200

    updated click logic.

--- src/test/java/automation/registration/Registration.java
+++ src/test/java/automation/registration/Registration.java
@@ -1,12 +1,13 @@
 package automation.registration;
 
-import automation.Constants;
-import automation.TestHelper;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
-import static org.junit.Assert.assertTrue;
+import automation.Constants;
+import automation.TestHelper;
 
 /**
  * Created by Julia on 22-Nov-15.
@@ -22,21 +23,26 @@ public class Registration extends TestHelper {
         window.get(Constants.SITE_URL);
 
         window.findElement(By.xpath("//a[contains(.,'Регистрация')]")).click();
-        pauseUntilDisplayed(By.xpath(".//*[@id='registration']//*[@id='user_models_User_phone']"),window);
+        pauseUntilDisplayed(By.xpath(".//*[@id='registration']//*[@id='user_models_User_phone']"), window);
         window.findElement((By.xpath(".//*[@id='registration']//*[@id='user_models_User_phone']"))).sendKeys(Constants.REG_LOGIN);
         window.findElement((By.xpath(".//*[@id='registration']//*[@id='user_models_User_password']"))).sendKeys(Constants.REG_PASSWORD);
-        window.findElement((By.xpath(".//*[@id='registration']//*[@id='user_models_User_rulesAccepted']"))).click();
-        pauseUntilDisplayed(By.id("registerButton"),window);
+        WebElement checkBox = window.findElement((By.xpath(".//*[@id='registration']//label//span")));
+        //наводим мишку и нажимаем
+        Actions builder = new Actions(window);
+        builder.moveToElement(checkBox, 5, 5).perform();
+        builder.click();
+
+        pauseUntilDisplayed(By.id("registerButton"), window);
         window.findElement(By.id("registerButton")).click();
 
         // вывод информации в консоли выполнения теста
         logger.info("Введите смс из телефона");
 
         //pauseUntilDisplayed подождать пока окно для ввода смс появилось
-        pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"),window);
+        pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"), window);
 
 
-        pauseUntilDisplayed(By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_phone']"),window);
+        pauseUntilDisplayed(By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_phone']"), window);
         window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_phone']"))).sendKeys(Constants.USER_LOGIN);
         window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_password']"))).sendKeys(Constants.USER_PASSWORD);
         window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_verifyPassword']"))).sendKeys(Constants.USER_PASSWORD);
@@ -46,12 +52,11 @@ public class Registration extends TestHelper {
         logger.info("Введите смс из телефона");
 
         //pauseUntilDisplayed подождать пока окно для ввода смс появилось
-        pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"),window);
+        pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"), window);
         window.findElement((By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"))).sendKeys("SMS ???????");
         //нажать подтвердить регистрацию
         //проверить что мы видим следующий шаг регистрации
     }
-
 
 
 }
