d20bdf9c03541e15dbcac41d78064836f286cfdd
Julia <loboda.yuliya@gmail.com>
2016-01-22 17:52:30 +0200

    edit class registration, add new constants for registration

--- src/test/java/automation/registration/Registration.java
+++ src/test/java/automation/registration/Registration.java
@@ -42,17 +42,6 @@ public class Registration extends TestHelper {
         pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"), window);
 
 
-        pauseUntilDisplayed(By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_phone']"), window);
-        window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_phone']"))).sendKeys(Constants.USER_LOGIN);
-        window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_password']"))).sendKeys(Constants.USER_PASSWORD);
-        window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_verifyPassword']"))).sendKeys(Constants.USER_PASSWORD);
-        window.findElement((By.xpath(".//div[@class='registration__body']//*[@id='user_modules_registration_models_User_rulesAccepted']"))).click();
-        window.findElement(By.id("add-action")).click();
-
-        logger.info("Введите смс из телефона");
-
-        //pauseUntilDisplayed подождать пока окно для ввода смс появилось
-        pauseUntilDisplayed(By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"), window);
         window.findElement((By.xpath("//*[@id='user_modules_registration_models_User_sms_access_code']"))).sendKeys("SMS ???????");
         //нажать подтвердить регистрацию
         //проверить что мы видим следующий шаг регистрации
