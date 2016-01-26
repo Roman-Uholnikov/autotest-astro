a56310d0a7602867226f1681ee80b7f3ac43a9c3
Julia <loboda.yuliya@gmail.com>
2016-01-26 18:53:20 +0200

    edit class registration, add new constants for registration

---
+++ src/test/java/automation/main_checklist/Notify_Me_Online.java
@@ -0,0 +1,64 @@
+package automation.main_checklist;
+
+import automation.Constants;
+import automation.TestHelper;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Julia on 26.01.2016.
+ */
+public class Notify_Me_Online extends TestHelper {
+
+    //проверить открытие карты эксперта оффлайн и нажать Заказать консультацию с главной, затем Уведомить меня
+
+    @Test
+    public void OpenCardOfflineExpert() {
+        WebDriver window = getNewWindow();
+        window.get(Constants.SITE_URL);
+
+        //найти эксперта со статусом Нет в сети и открыть большую карточку (по кнопке Заказать консультацию)
+
+        if (window.findElements(By.xpath("//*[@class='btn btn-green btn-alpha']")).size() > 0) {
+            window.findElements(By.xpath("//*[@class='btn btn-green btn-alpha']")).get(0).click();
+        }
+
+        //pauseUntilDisplayed подождать пока окно большой карты Эксперта появилось
+        pauseUntilDisplayed(By.xpath("//*[@class='product__title']"), window);
+
+        //кликнуть по кнопке Уведомить меня
+        window.findElement(By.xpath("//*[@class='btn btn-default btn-lg product__order-button ga_btn']")).click();
+
+        //войти на сайт использовать метод логина из тест хелпера
+        loginSiteUser(Constants.USER_LOGIN,Constants.USER_PASSWORD,window);
+
+        //открылось окно с инфо об уведомлении с номером телефона юзера
+        pauseUntilDisplayed(By.xpath(".//*[@id='notify-me-online']"), window);
+
+        //проверить что отображается телефон
+    }
+
+
+
+    //проверить открытие карточки Занят на линии и кликнуть Уведомить меня в маленькой карте Эксперта
+
+    @Test
+    public void OpenCardBusyExpert() {
+        WebDriver window = getNewWindow();
+        window.get(Constants.SITE_URL);
+
+        //найти эксперта со статусом Занят на линии и кликнуть по кнопке Уведомить о доступности
+
+        if (window.findElements(By.xpath("//*[@class='']")).size() > 0) {
+            window.findElements(By.xpath("//*[@class='btn btn-green btn-alpha']")).get(0).click();
+        }
+
+        //pauseUntilDisplayed подождать пока окно большой карты Эксперта появилось
+        pauseUntilDisplayed(By.xpath("//*[@class='product__title']"), window);
+
+        //нажать на
+}
+
+
+}
