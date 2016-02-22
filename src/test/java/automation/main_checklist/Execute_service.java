8d6cd3a909b35e3d2a1c198061108d829546befc
Julia <loboda.yuliya@gmail.com>
2016-02-22 18:55:10 +0200

    реализация метода Заказ/выполнение услуги

--- src/test/java/automation/main_checklist/Execute_service.java
+++ src/test/java/automation/main_checklist/Execute_service.java
@@ -1,20 +1,41 @@
 package automation.main_checklist;
 
+import automation.Constants;
+import automation.TestHelper;
+import com.sun.java.swing.plaf.windows.resources.windows;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import sun.plugin2.os.windows.Windows;
+
+import java.awt.*;
+
 /**
  * Created by Julia on 26.01.2016.
  */
-public class Execute_service {
+public class Execute_service extends TestHelper {
 
-    //зайти под клиентом
-    // заказать услугу у эксперта
-    //пополнить счет - редирект
-    //заказ
-    //выйти из пользователя
-    //зайти под экспертом
-    //выполнить услугу
-    //выйти из эксперта
-    //зайти под модератором
-    //пропустить услугу
-    //зайти под клиентом
-    //скачать пдф с услугой, ПРОВЕРИТЬ текст
+
+    @Test
+    public void Order_service() {
+        WebDriver window = getNewWindow();
+        window.get(Constants.SITE_URL);
+
+        //войти под Клиентом
+        loginSiteUser(Constants.USER_LOGIN,Constants.USER_PASSWORD,window);
+        //открыть карточку тестового эксперта Юли
+        window.get("https://astro.club/expert/yuli");
+
+
+        // заказать услугу у эксперта
+        //пополнить счет - редирект
+        //заказ
+        //выйти из пользователя
+        //зайти под экспертом
+        //выполнить услугу
+        //выйти из эксперта
+        //зайти под модератором
+        //пропустить услугу
+        //зайти под клиентом
+        //скачать пдф с услугой, ПРОВЕРИТЬ текст
+    }
 }
